08bf238ba4f2d7d4c623752e3f8698cca284f2ab
EnzoCam7 <camerac09@gmail.com>
2023-02-12 20:18:36 -0300

    Updates

--- components/Nav.js
+++ components/Nav.js
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 export default function Nav() {
   return (
-    <nav className="flex justify-between items-center py-6">
+    <nav className="flex justify-between items-center py-7">
       <Link href={'/'}>
         <button className="text-2xl font-medium">CreaMinds</button>
       </Link>
 
       <ul>
-        <Link href={'/auth/login'} className="text-xl font-semibold bg-gray-700 text-white p-2 rounded">
+        <Link href={'/auth/login'} className="text-xl font-semibold bg-stone-900 text-white p-2 rounded">
           Join Now
         </Link>
       </ul>
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,19 +1,19 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase'
-import {getFireStore} from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
+import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
+//If we are using NEXT, the ENV's has to have the NEXT_PUBLIC name first.
 const firebaseConfig = {
-  apiKey: process.env.DB_APIKEY,
-  authDomain: process.env.DB_AUTH_DOMAIN,
-  projectId: process.env.DB_PROJECT_ID,
-  storageBucket: process.env.DB_STORAGE_BUCKET,
-  messagingSenderId: process.env.DB_MESSAGING_SENDER,
-  appId: process.env.DB_APP_ID
+  apiKey : process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth()
-//
-export const db = getFireStore(app)
+export const db = getFirestore(app)
